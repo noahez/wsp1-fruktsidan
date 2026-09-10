f1c859a861631b006b5b2323b790a1b2053cb757
Noah Esbold Zachrisson <noah.esboldzachrisson@elev.ga.ntig.se>
2026-09-10 15:34:41 +0200

    v2

--- app.rb
+++ app.rb
@@ -16,7 +16,14 @@ get '/fruits' do
    @fruits = db.execute('SELECT * FROM products')
   ap @fruits
 erb(:"fruits/index")
+
+
  # Ruby koden för vad som ska vara i HTTP response här
+end
+get '/fruits/:id' do | id |
+@fruit = db.execute('SELECT * FROM products WHERE id=?',id).first 
+ap @fruit
+erb(:"fruits/show")
 end
     #TODO: Skriv routen hämtar alla frukter i databasen
 
